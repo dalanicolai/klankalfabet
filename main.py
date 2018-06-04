853f00488fd963802ba69bacfdf174c83abab1ac
Daniel Nicolai <dalanicolai@gmail.com>
2018-06-04 19:25:05 +0200

    Nieuwe versie (Ronaldo is een sukkel)

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 import kivy
 kivy.require('1.10.0')
 
+from gtts import gTTS
+import os
+
 from kivy.core.audio import SoundLoader
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import Image
 
 class KlankalfabetApp(App):
+    # def __init__(self):
+    #     self.img_source = "/images/sukkel.jpg"
 
     def build(self):
-        return GridLayout()
+        return BoxLayout()
 
     def key_pressed(self, letter):
         sound = SoundLoader.load(letter)
         sound.play()
+
+    def playstring(self, text):
+        if text is '':
+            text = "ronaldo is een sukkel"
+            self.img_source = "/images/sukkel.jpg"
+        tts = gTTS(text, lang='nl')
+        tts.save("good.mp3")
+        os.system("mpg123 good.mp3")
 
 klankalfabet = KlankalfabetApp()
 
