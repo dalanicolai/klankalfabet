b9cf1c4631eff22c962b4f4cd15504088bbcdf74
Daniel Nicolai <dalanicolai@gmail.com>
2018-06-05 03:49:11 +0200

    AFBEELDING WIJZIGEN WERKT!!!

--- main.py
+++ main.py
@@ -7,25 +7,32 @@ import os
 from kivy.core.audio import SoundLoader
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-# from kivy.uix.image import Image
+from kivy.properties import ObjectProperty
 
-class KlankalfabetApp(App):
-    # img_source = 'images/art.png'
+class MainWindow(BoxLayout):
+    img_source = ObjectProperty(id)
 
-    def build(self):
-        return BoxLayout()
+    def img_path(self):
+        print(self.img_source.source)
 
-    def key_pressed(self, letter):
-        sound = SoundLoader.load(letter)
-        sound.play()
 
     def playstring(self, text):
         if text is '':
             text = "ronaldo is een sukkel"
+            self.img_source.source = 'images/sukkel.jpg'
+            self.img_source.reload()
         tts = gTTS(text, lang='nl')
         tts.save("good.mp3")
         os.system("mpg123 good.mp3")
-        return
+
+class KlankalfabetApp(App):
+
+    def build(self):
+        pass
+
+    def key_pressed(self, letter):
+        sound = SoundLoader.load(letter)
+        sound.play()
 
 klankalfabet = KlankalfabetApp()
 
